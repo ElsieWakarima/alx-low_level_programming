49db8f1e6122b093efc3e49f77ebe4812c09184e
ElsieWakarima <96049425+ElsieWakarima@users.noreply.github.com>
2022-09-09 15:15:10 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,17 @@
-f
+#include <stdio.h>
+#include <string.h>
+
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+char displayed[52];
+
+strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+puts(displayed);
+return (0);
+}
